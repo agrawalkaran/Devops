2a795f1ca430ec564e4831a47f5e8ee629c9a22d
Karankumar Agrawal <karan.cba1750@ict.gnu.ac.in>
2020-01-20 03:17:49 +0000

    commit2

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Ganpat Changes by karan</p>
+        <p className="p-3 text-light h3 bg-primary"> Karan Changes </p>
     </div>
 }
   
